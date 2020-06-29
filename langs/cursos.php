3fe59eadf4288a781eb91ef7aeebb347caf6b7de
Antony Charles <charles_aclr@yahoo.com.br>
2020-06-28 21:13:29 -0300

    remoção de arquivos php

--- langs/cursos.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'title' => 'Outros',
-
-    'titleFormacaoC' => 'Formação .NET - Alura',
-];
--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
-class Header{
+const header = {
 	fixedHeaderScroll(){
 		if (window.pageYOffset > 100) {
 			document.getElementById("header-top").classList.add("fixed");
 		} else {
 			document.getElementById("header-top").classList.remove("fixed");
 		}
-	}
+	},
 
 	scrollLink(ahref,ahrefEvent){
 		ahrefEvent.preventDefault();
@@ -17,7 +17,7 @@ class Header{
 		if(ahref.dataset.logo == undefined){
 			document.querySelector('#header-button').click();
 		}
-	}
+	},
 
 	changeMenuMobile(){
 		let itens = document.querySelector(".header-mobile-show");
@@ -32,18 +32,19 @@ class Header{
 		}
 	}
 }
+window.onscroll = function() {header.fixedHeaderScroll();};
 
-class Modal{
+const modal = {
     openModal(button){
         document.getElementById(button.dataset.modal).style.display = "block";
-    }
+    },
 
     closeModal(close){
         let modalOpen = close.parentElement;
         if(modalOpen.parentElement.classList.contains('modal')){
             modalOpen.parentElement.style.display = "none";
         }
-    }
+    },
 
     closeModalOnclickWindow(event){
         if(event.target.classList.contains('modal')){
@@ -51,8 +52,10 @@ class Modal{
         }
     }
 }
+window.onclick = function(event){modal.closeModalOnclickWindow(event);}
 
-class Accordion{
+
+const accordion = {
     changePanel(button){
 		button.classList.toggle("active");
         let panel = button.nextElementSibling;
@@ -64,48 +67,44 @@ class Accordion{
     }
 }
 
-var accordion = new Accordion();
-
-var header = new Header();
-window.onscroll = function() {header.fixedHeaderScroll();};
-
-var modal = new Modal();
-window.onclick = function(event){modal.closeModalOnclickWindow(event);}
-
-document.querySelector("body").onload = function() {
-	var c = document.getElementById("animacao");
-	var ctx = c.getContext("2d");
-
-	c.height = document.getElementById("banner").offsetHeight;
-	c.width = document.getElementById("banner").offsetWidth;
-
-	var chinese = "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑";
-	chinese = chinese.split("");
-
-	var font_size = 16;
-	var columns = c.width/font_size;
-	var drops = [];
-
-	for(var x = 0; x < columns; x++)
-		drops[x] = 1; 
-
-	function draw()
-	{
-		ctx.fillStyle = "rgba(122, 130, 171, 0.2)";
-		ctx.fillRect(0, 0, c.width, c.height);
+const bannerMatrix = {
+	constructor(){
+		this.palco = document.getElementById("animacao");
+		this.ctx = this.palco.getContext("2d");
+	
+		this.palco.height = document.getElementById("banner").offsetHeight;
+		this.palco.width = document.getElementById("banner").offsetWidth;
+	
+		this.chinese = "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑";
+		this.chinese = this.chinese.split("");
+	
+		this.font_size = 16;
+		this.columns = this.palco.width/this.font_size;
+		this.drops = [];
+	
+		for(var x = 0; x < this.columns; x++)
+			this.drops[x] = 1; 
+	},
+	
+	draw(){
+		this.ctx.fillStyle = "rgba(122, 130, 171, 0.2)";
+		this.ctx.fillRect(0, 0, this.palco.width, this.palco.height);
 		
-		ctx.fillStyle = "#FFF";
-		ctx.font = font_size + "px arial";
-		for(var i = 0; i < drops.length; i++)
+		this.ctx.fillStyle = "#FFF";
+		this.ctx.font = this.font_size + "px arial";
+		for(var i = 0; i < this.drops.length; i++)
 		{
-			var text = chinese[Math.floor(Math.random()*chinese.length)];
-			ctx.fillText(text, i*font_size, drops[i]*font_size);
-			if(drops[i]*font_size > c.height && Math.random() > 0.975)
-				drops[i] = 0;
+			let text = this.chinese[Math.floor(Math.random()*this.chinese.length)];
+			this.ctx.fillText(text, i*this.font_size, this.drops[i]*this.font_size);
+			if(this.drops[i]*this.font_size > this.palco.height && Math.random() > 0.975)
+				this.drops[i] = 0;
 			
-			drops[i]++;
+			this.drops[i]++;
 		}
 	}
+}
 
-	setInterval(draw, 50);
+document.querySelector("body").onload = function() {
+	bannerMatrix.constructor();
+	setInterval(function(){bannerMatrix.draw();}, 50);
 };
